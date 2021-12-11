e86e745158c133cf6f4746f5dadbc223809d26aa
Nofar Alon <nofaralon28@gmail.com>
2021-12-11 21:57:18 +0200

    fixed socket

--- services/socket.service.js
+++ services/socket.service.js
@@ -31,16 +31,7 @@ function connectSockets(http, session) {
                 // emits only to sockets in the same room
                 // gIo.to(socket.myTopic).emit('chat addMsg', msg)
         })
-        socket.on('user-watch', userId => {
-            socket.join('watching:' + userId)
-        })
-        socket.on('set-user-socket', userId => {
-            logger.debug(`Setting (${socket.id}) socket.userId = ${userId}`)
-            socket.userId = userId
-        })
-        socket.on('unset-user-socket', () => {
-            delete socket.userId
-        })
+
 
     })
 }
